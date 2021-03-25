6567141edf7d53f0ac2465225f1f7b9dd5086ed2
Brandon Perard <brandon.perard@gmail>
2021-03-25 13:28:41 -0700

    add test to app.test.js for endpoint to get all orders

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -37,4 +37,28 @@ describe('03_separation-of-concerns-demo routes', () => {
       quantity: 10,
     });
   });
+
+  it('gets all orders in our database', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 10 });
+
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 33 });
+
+    const res = await request(app)
+      .get('/api/v1/orders');
+
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        quantity: 10,
+      },
+      {
+        id: '2',
+        quantity: 33,
+      },
+    ]);
+  });
 });
